a8490e13bcda61a5dae0e0e3c22fdf96ce6a80b0
AsianIntel <gautam.abhyankar@gmail.com>
2024-06-15 16:06:56 -0700

    slight docs changes

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -11,7 +11,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
         <MenuItems />
       </MobileMenu>
 
-      <div className="col-span-3 prose my-20 mx-auto prose-headings:mt-8 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h2:text-blue-600 prose-h3:text-3xl prose-h3:text-blue-600 prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-headings:text-gray-500 prose-p:text-slate-300">
+      <div className="col-span-3 prose max-w-full my-20 prose-headings:mt-8 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-headings:text-gray-200 prose-p:text-white prose-a:text-blue-600 prose-a:no-underline">
         {children}
       </div>
     </div>
