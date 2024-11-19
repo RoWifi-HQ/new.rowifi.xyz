075c8b7cac085c3f796a326ac09a15dd0579051f
AsianIntel <gautam.abhyankar@gmail.com>
2024-11-18 21:55:42 -0800

    more doc pages

--- src/app/docs/components.tsx
+++ src/app/docs/components.tsx
@@ -312,25 +312,28 @@ function MenuItems() {
           href="/docs/tower/setup"
           className="w-full lg:w-2/3 flex justify-center lg:justify-start items-center hover:text-gray-400"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+          </svg>
           <span className="ml-2 font-medium">Setup</span>
         </Link>
         <Link
           href="/docs/tower/binds"
           className="w-full lg:w-2/3 flex justify-center lg:justify-start items-center hover:text-gray-400"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+          </svg>
           <span className="ml-2 font-medium">XP Binds</span>
         </Link>
         <Link
           href="/docs/tower/commands"
           className="w-full lg:w-2/3 flex justify-center lg:justify-start items-center hover:text-gray-400"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+          </svg>
           <span className="ml-2 font-medium">Commands</span>
-        </Link>
-        <Link
-          href="/docs/tower/commands"
-          className="w-full lg:w-2/3 flex justify-center lg:justify-start items-center hover:text-gray-400"
-        >
-          <span className="ml-2 font-medium">Events</span>
         </Link>
       </section>
 
@@ -406,6 +409,9 @@ function MenuItems() {
           href="/docs/roles"
           className="w-full lg:w-2/3 flex justify-center lg:justify-start items-center hover:text-gray-400"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>
           <span className="ml-2 font-medium">Auto Detection</span>
         </Link>
       </section>
